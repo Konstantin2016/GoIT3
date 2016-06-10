06e7d0480887f82eeefc9f4ec33a7478db4fced7
Середюк Костянтин Валентинович <KSeredyuk@branches.icu.int>
2016-06-10 18:52:50 +0300

    third commit

---
+++ OfflineSalary/PerMonthEmployee.java
@@ -0,0 +1,7 @@
+package OfflineSalary;
+
+/**
+ * Created by KSeredyuk on 06.06.2016.
+ */
+public class PerMonthEmployee {
+}
---
+++ powerMath/power.java
@@ -0,0 +1,7 @@
+package powerMath;
+
+/**
+ * Created by KSeredyuk on 06.06.2016.
+ */
+public class power {
+}
